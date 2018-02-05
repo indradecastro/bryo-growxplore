d49c961296d7b5a30ba5690fea327486dcaa2abb
indradecastro <indradatta@wanadoo.fr>
2018-02-05 10:24:27 +0100

    ahora si

---
+++ server.R
@@ -0,0 +1,27 @@
+library(shiny)
+raw <- read.table("first/20180124.eval.cualitativa.csv", header=T, na.strings="", sep="\t")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output){
+      
+      moss <- reactive({as.character(input$moss)})
+      
+  output$adults <- renderPlot({
+    # subset data based on input$moss from ui.R
+        adults <- raw[which(raw$tipo=="adult" & raw$sp==agrep(pattern=moss, x=raw$sp, value=T)),]
+        
+    # draw the histogram with the specified number of bins
+    barplot(table(adults)/nrow(sp)*100, ylim=c(0,100), ylab="percentage of samples")
+           legend(legend=c("A: Growth", "B: Same", "C: Damage <50%", "D: Damage >50%"), title="Growth & Damage", "topright", bty="n", cex=0.5)
+  })
+  
+  output$pures <- renderPlot({
+        # subset data based on input$moss from ui.R
+        pures <- raw[which(raw$tipo=="pure" & raw$sp==agrep(pattern=moss, x=raw$sp, value=T)),]
+        
+        # draw the histogram with the specified number of bins
+        barplot(table(pures)/nrow(sp)*100, ylim=c(0,100), ylab="percentage of samples")
+               legend(legend=c("0: zero"), title="Growth percentage", "topright", bty="n", cex=0.5)
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("By Species"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+        sidebarPanel(
+              selectInput("moss",
+                          "Select a moss species:",
+                          c("Dicranum", "Pleurochaete", "Homalothecium",
+                            "Hypnum", "Syntrichia", "Bryum"),
+                          selected = "Dicranum")
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(fluidRow(splitLayout(cellWidths = c("50%", "50%"),
+                                   plotOutput("adults"),
+                                   plotOutput("pures")))
+        )
+  )
+))
